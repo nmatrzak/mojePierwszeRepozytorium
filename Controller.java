0c589a5d7da2f683785c11cf4e3b49a6b1fffad1
nmatrzak <37778736+nmatrzak@users.noreply.github.com>
2018-07-21 16:27:34 +0200

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,39 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Controller {
+
+	@FXML
+	private Button button;
+	@FXML
+	private TextField tx;
+	@FXML
+	private TextField tx1;
+	@FXML
+	private Label lable;
+	
+	 @FXML
+	 private void initialize() {
+		
+		 
+	 }
+	 public void setParameter() {
+		tx.setText("0.0");
+		tx1.setText("0");
+		
+		
+	 }
+	 
+	 public void multi_Clik(ActionEvent event) {
+		 double result = Double.parseDouble(tx.getText())*Double.parseDouble(tx1.getText());
+		 lable.setText(Double.toString(result));
+	 }
+}
